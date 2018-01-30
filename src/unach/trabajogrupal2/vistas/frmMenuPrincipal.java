c139b9c3024c150a964dd80c9d1e4dc53ed29c0c
CRISTIAN <CRISTIAN@LAPTOP-5MJUSNOD.mshome.net>
2018-01-30 16:54:50 -0500

    actualizacion de conexion

--- src/unach/trabajogrupal2/vistas/frmMenuPrincipal.java
+++ src/unach/trabajogrupal2/vistas/frmMenuPrincipal.java
@@ -1,77 +1,178 @@
 package unach.trabajogrupal2.vistas;
+
 import java.awt.BorderLayout;
-import java.awt.GridLayout;
+import java.awt.Color;
+import static java.awt.Frame.MAXIMIZED_BOTH;
+import java.awt.PopupMenu;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import javax.swing.*;
-public class frmMenuPrincipal extends JFrame {
-JLabel lblTitulo0;
-JLabel lblTitulo1;
-JLabel lblTitulo2;
-JLabel lblTitulo3;
-JLabel lblGenero;
-JTextField txtTitulo0;
-JTextField txtTitulo1;
-JTextField txtTitulo2;
-JTextField txtTitulo3;
-JComboBox cmbGenero;
-JButton btnLimpiar;
-JButton btnAceptar;
+import static javax.swing.JFrame.EXIT_ON_CLOSE;
 
-JPanel pnlCentral;
-JPanel pnlPie;
+public class frmMenuPrincipal extends JFrame {
+
+    JMenuBar mnbPrincipal;
+    JMenu mnInicio;
+    JMenuItem mniLogin;
+    JMenuItem mniSalir;
+    JMenu mnIngreso;
+    JMenuItem mniNuevoIngreso;
+    JMenuItem mniModificaIngreso;
+    JMenuItem mniEliminaIngreso;
+    JMenuItem mniBuscaIngreso;
+    JMenuItem mniListaIngreso;
+    JMenu mnEgreso;
+    JMenuItem mniNuevoEgreso;
+    JMenuItem mniModificaEgreso;
+    JMenuItem mniEliminaEgreso;
+    JMenuItem mniBuscaEgreso;
+    JMenuItem mniListaEgreso;
+    JMenu mnCliente;
+    JMenuItem mniNuevoCliente;
+    JMenuItem mniModificaCliente;
+    JMenuItem mniEliminaCliente;
+    JMenuItem mniBuscaCliente;
+    JMenuItem mniListaCliente;
+    JDesktopPane dkpEscritorio;
+
     public frmMenuPrincipal() {
-        this.setSize(200,200);
-        this.setLayout(new BorderLayout());
-        pnlCentral=new JPanel();
-        pnlPie=new JPanel();
-        pnlCentral.setLayout(new GridLayout(4,2,5,5));
-        pnlPie.setLayout(new GridLayout(1,2,5,5));
-        
-        lblTitulo0=new JLabel("Datos Estudiante");
-        
-        lblTitulo1=new JLabel("Etiqueta1");
-        lblTitulo2=new JLabel("Etiqueta2");
-        lblTitulo3=new JLabel("Etiqueta3");
-        lblGenero= new JLabel("Genero");
-        txtTitulo1=new JTextField(2);
-        txtTitulo2=new JTextField(2);
-        txtTitulo3=new JTextField(2);
-        cmbGenero=new JComboBox(new String[]{"Masculino","Femenino"});
-        btnLimpiar=new JButton("Limpiar");
-        btnAceptar=new JButton("Aceptar");
-        
-        pnlCentral.add(lblTitulo1);
-        pnlCentral.add(txtTitulo1);
-        pnlCentral.add(lblTitulo2);
-        pnlCentral.add(txtTitulo2);
-        pnlCentral.add(lblTitulo3);
-        pnlCentral.add(txtTitulo3);
-        pnlCentral.add(lblGenero);
-        pnlCentral.add(cmbGenero);
-        
-        btnAceptar.addActionListener(new ActionListener() {
+        dkpEscritorio = new JDesktopPane();
+        dkpEscritorio.setBackground(new Color(240, 240, 240));
+        mnbPrincipal = new JMenuBar();
+        mnInicio = new JMenu("Inicio");
+        mniLogin = new JMenuItem("Iniciar Secion");
+        mniSalir = new JMenuItem("Salir");
+        mniSalir.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                try{
-                    btnAceptarActionListener(e);
-                }catch(Exception ex){
-                    System.out.println("Error"+ex.getMessage());
-                }
+                mniSalirActionPerformed(e);
             }
         });
-      
-        pnlPie.add(btnLimpiar);
-        pnlPie.add(btnAceptar);
-        this.add(lblTitulo0,BorderLayout.NORTH);
-        this.add(pnlCentral,BorderLayout.CENTER);
-        this.add(pnlPie,BorderLayout.SOUTH);
+        mnIngreso = new JMenu("Ingreso");
+        mnEgreso = new JMenu("Egreso");
+        mnCliente = new JMenu("Cliente");
+        mniNuevoIngreso = new JMenuItem("Nuevo");
+//        mniNuevoIngreso.addActionListener(new ActionListener() {
+//            @Override
+//            public void actionPerformed(ActionEvent e) {
+//                mniNuevoIngresoActionPerformed(e);
+//            }
+//        });
+        mniModificaIngreso = new JMenuItem("Modifica");
+        mniEliminaIngreso = new JMenuItem("Elimina");
+        mniBuscaIngreso = new JMenuItem("Busca");
+        mniListaIngreso = new JMenuItem("Lista");
+        
+//        mniListaEstudiante.addActionListener(new ActionListener() {
+//            @Override
+//            public void actionPerformed(ActionEvent e) {
+//                mniListaEstudianteActionPerdormed(e);
+//            }
+//        });
+
+        mniNuevoEgreso = new JMenuItem("Nuevo");
+//        mniNuevoEgreso.addActionListener(new ActionListener() {
+//            @Override
+//            public void actionPerformed(ActionEvent e) {
+//                mniNuevoEgresoActionPerformed(e);
+//            }
+//        });
+        mniModificaEgreso = new JMenuItem("Modifica");
+        mniEliminaEgreso = new JMenuItem("Elimina");
+        mniBuscaEgreso = new JMenuItem("Busca");
+        mniListaEgreso = new JMenuItem("Lista");
+        
+        mniNuevoCliente = new JMenuItem("Nuevo");
+        mniNuevoCliente.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                mniClienteActionPerformed(e);
+            }
+        });
+        mniModificaCliente = new JMenuItem("Modifica");
+        mniEliminaCliente = new JMenuItem("Elimina");
+        mniBuscaCliente = new JMenuItem("Busca");
+        mniListaCliente = new JMenuItem("Lista");
+        
+//        mniListaDocente.addActionListener(new ActionListener() {
+//            @Override
+//            public void actionPerformed(ActionEvent e) {
+//                mniListaDocenteActionPerdormed(e);
+//            }
+//        });
+        
+        mnIngreso.add(mniNuevoIngreso);
+        mnIngreso.add(mniModificaIngreso);
+        mnIngreso.add(mniEliminaIngreso);
+        mnIngreso.addSeparator();
+        mnIngreso.add(mniBuscaIngreso);
+        mnIngreso.add(mniListaIngreso);
+        mnbPrincipal.add(mnInicio);
+        mnbPrincipal.add(mnIngreso);
+        mnbPrincipal.add(mnEgreso);
+        mnbPrincipal.add(mnCliente);
+        mnInicio.add(mniLogin);
+        mnInicio.add(mniSalir);
+
+        mnEgreso.add(mniNuevoEgreso);
+        mnEgreso.add(mniModificaEgreso);
+        mnEgreso.add(mniEliminaEgreso);
+        mnEgreso.addSeparator();
+        mnEgreso.add(mniBuscaEgreso);
+        mnEgreso.add(mniListaEgreso);
+
+        mnCliente.add(mniNuevoCliente);
+        mnCliente.add(mniModificaCliente);
+        mnCliente.add(mniEliminaCliente);
+        mnCliente.addSeparator();
+        mnCliente.add(mniBuscaCliente);
+        mnCliente.add(mniListaCliente);
+        
+        this.setLayout(new BorderLayout());
+        this.add(mnbPrincipal, BorderLayout.NORTH);
+        this.add(dkpEscritorio, BorderLayout.CENTER);
+        this.setExtendedState(MAXIMIZED_BOTH);
+        this.setDefaultCloseOperation(EXIT_ON_CLOSE);
     }
-public static void main(String[] args){
-    frmMenuPrincipal frmMenu = new frmMenuPrincipal();
-    frmMenu.setVisible(true);
-}
-public void btnAceptarActionListener(ActionEvent e){
-    JOptionPane.showMessageDialog(this, "Proceso correcto!", "Transaccion",JOptionPane.INFORMATION_MESSAGE);
-}
+
+    public void mniSalirActionPerformed(ActionEvent e) {
+        System.exit(0);
+    }
+
+//    public void mniNuevoEstudianteActionPerformed(ActionEvent e) {
+//        frmNuevoEstudiante frm = new frmNuevoEstudiante();
+//        dkpEscritorio.add(frm);
+//        frm.setVisible(true);
+//    }
+//
+//    public void mniNuevoCursoActionPerformed(ActionEvent e) {
+//        frmNuevoCurso frm = new frmNuevoCurso();
+//        dkpEscritorio.add(frm);
+//        frm.setVisible(true);
+//    }
+//
+    public void mniClienteActionPerformed(ActionEvent e) {
+        frmCliente frm = new frmCliente();
+        dkpEscritorio.add(frm);
+        frm.setVisible(true);
+    }
+    
+
+//    public void mniListaEstudianteActionPerdormed(ActionEvent e)
+//    {
+//        frmListaEstudiante frm = new frmListaEstudiante();
+//        dkpEscritorio.add(frm);
+//        frm.setVisible(true);
+//    }
+//    public void mniListaDocenteActionPerdormed(ActionEvent e)
+//    {
+//        frmListaDocente frm = new frmListaDocente();
+//        dkpEscritorio.add(frm);
+//        frm.setVisible(true);
+//    }
+    public static void main(String[] args) {
+        frmMenuPrincipal frmMEnu = new frmMenuPrincipal();
+        frmMEnu.setVisible(true);
+
+    }
 }
