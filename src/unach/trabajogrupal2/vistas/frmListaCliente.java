d942024ca481501017fcd289ca32213ada929cd3
Usuario <Usuario@172.30.3.200>
2018-01-30 17:37:19 -0500

    Actualizacion de las librerias

--- src/unach/trabajogrupal2/vistas/frmListaCliente.java
+++ src/unach/trabajogrupal2/vistas/frmListaCliente.java
@@ -1,10 +1,17 @@
 
 package unach.trabajogrupal2.vistas;
 
+import java.awt.BorderLayout;
+import java.util.ArrayList;
+import java.util.List;
 import javax.swing.JInternalFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JTable;
 import javax.swing.table.DefaultTableModel;
+import unach.trabajogrupal2.dao.ICliente;
+import unach.trabajogrupal2.entidades.Cliente;
+import unach.trabajogrupal2.impl.ImplCliente;
 
 
 public class frmListaCliente extends JInternalFrame {
@@ -12,4 +19,40 @@ public class frmListaCliente extends JInternalFrame {
     JTable tabla;
     DefaultTableModel modelo;
     
+
+public frmListaCliente(){
+    this.setSize(800,600);
+    this.setLayout(new BorderLayuot());
+    this.setClosable(true);
+    
+    
+   lbTitulo = new JLabel("Listado de Clientes");
+   tabla = new JTable();
+   this.add(lbTitulo,BorderLayout.NORTH);
+   this.add(tabla,BorderLayout.CENTER);
+   cargarTabla();
+   
+  
+
+}
+public void cargarTabla(){
+
+modelo= new  DefaultTableModel();
+modelo.addColumn("Codigo");
+modelo.addColumn("Cedula");
+modelo.addColumn("Nombre");
+modelo.addColumn("Apellido");
+ List<Cliente>lista = new ArrayList<>();
+ try{
+     ICliente clienteDao= new ImplCliente();
+     lista=clienteDao.obtener();
+ }catch(Exception e){
+     JOptionPane.showMessageDialog(this,e.getMessage(),"Error",JOptionPane.ERROR_MESSAGE);
+ }
+ for (Cliente cl : lista) {
+     modelo.addRow(new Object[]{cl.getCodigocl(),cl.getNombre(),cl.getApellido(),cl.getCedula()});
+     
+ }
+ tabla.setModel(modelo);
+}
 }
