33cd1a91631b3579a6a91430cb4a724a61342ef6
Usuario <Usuario@172.30.3.200>
2018-01-30 17:07:43 -0500

    Actualizacion de las librerias

--- src/unach/trabajogrupal2/vistas/frmCliente.java
+++ src/unach/trabajogrupal2/vistas/frmCliente.java
@@ -1,4 +1,3 @@
-
 package unach.trabajogrupal2.vistas;
 
 import unach.trabajogrupal2.entidades.*;
@@ -10,15 +9,14 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import javax.swing.*;
 
-public class frmCliente extends JFrame { 
-    
+public class frmCliente extends JFrame {
+
     JLabel titulo;
     JLabel codigocl;
     JLabel nombres;
     JLabel apellidos;
     JLabel cedula;
-  
-    
+
     JTextField txttitulo1;
     JTextField txttitulo;
     JTextField txttitulo2;
@@ -26,11 +24,10 @@ public class frmCliente extends JFrame {
     JTextField txtnombres;
     JTextField txtapellidos;
     JTextField txtcedula;
-    
-   
+
     JButton btnLimpiar;
     JButton btnAceptar;
-    
+
     JPanel pnlcentral;
     JPanel pnlpie;
 
@@ -42,43 +39,42 @@ public class frmCliente extends JFrame {
 
         pnlcentral.setLayout(new GridLayout(12, 2, 5, 5));
         pnlpie.setLayout(new GridLayout(1, 2, 5, 5));
-        
-        
-       
+
         titulo = new JLabel("                                             DATOS DEL DOCENTE..   ");
         codigocl = new JLabel("CODIGO: ");
-        txtcedula = new JTextField(2);
+        txtcodigocl = new JTextField(2);
         nombres = new JLabel("NOMBRE: ");
         txtnombres = new JTextField(2);
         apellidos = new JLabel("APELLIDO: ");
         txtapellidos = new JTextField(2);
         cedula = new JLabel("CEDULA: ");
-        
-    
+        txtcedula = new JTextField(2);
+
         btnLimpiar = new JButton("LIMPIAR");
         btnAceptar = new JButton("ACEPTAR");
-        
+
         this.add(titulo, BorderLayout.NORTH);
-        
-        pnlcentral.add(titulo);  
+
+        pnlcentral.add(titulo);
         pnlcentral.add(codigocl);
-        pnlcentral.add(txtcedula);
-        pnlcentral.add(nombres); 
+        pnlcentral.add(txtcodigocl);
+        pnlcentral.add(nombres);
         pnlcentral.add(txtnombres);
         pnlcentral.add(apellidos);
         pnlcentral.add(txtapellidos);
         pnlcentral.add(cedula);
-        
+        pnlcentral.add(txtcedula);
+
         pnlpie.add(btnLimpiar);
         pnlpie.add(btnAceptar);
-        
+
         btnAceptar.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 try {
                     btnAceptarActionListener(e);
                 } catch (Exception ex) {
-                    System.out.print("error "+ex.getMessage());
+                    System.out.print("error " + ex.getMessage());
                 }
             }
         });
@@ -87,16 +83,17 @@ public class frmCliente extends JFrame {
         this.add(pnlpie, BorderLayout.SOUTH);
 
     }
-      public static void main(String[] args) {
-        
+
+    public static void main(String[] args) {
+
         frmCliente frmMenu2 = new frmCliente();
-        frmMenu2.setVisible(true);   
+        frmMenu2.setVisible(true);
     }
-    public void btnAceptarActionListener(ActionEvent e){
+
+    public void btnAceptarActionListener(ActionEvent e) {
 //        IDocente docenteDao =new DocenteImpl;
 //        
-        
-        
-        JOptionPane.showMessageDialog(this, "PROCESO CORRECTO!!", "Transaction",JOptionPane.INFORMATION_MESSAGE);
+
+        JOptionPane.showMessageDialog(this, "PROCESO CORRECTO!!", "Transaction", JOptionPane.INFORMATION_MESSAGE);
     }
 }
--- src/unach/trabajogrupal2/vistas/frmListaCliente.java
+++ src/unach/trabajogrupal2/vistas/frmListaCliente.java
@@ -1,7 +1,15 @@
 
 package unach.trabajogrupal2.vistas;
 
+import javax.swing.JInternalFrame;
+import javax.swing.JLabel;
+import javax.swing.JTable;
+import javax.swing.table.DefaultTableModel;
 
-public class frmListaCliente {
+
+public class frmListaCliente extends JInternalFrame {
+    JLabel lbTitulo;
+    JTable tabla;
+    DefaultTableModel modelo;
     
 }
