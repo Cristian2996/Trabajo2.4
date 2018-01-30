e3e9601b1ae7a43bfb3295032755053349c1e0cd
Usuario <Usuario@172.30.3.200>
2018-01-30 17:06:59 -0500

    Actualizacion de las librerias

---
+++ src/unach/trabajogrupal2/vistas/frmNuevoCliente.java
@@ -0,0 +1,100 @@
+package unach.trabajogrupal2.vistas;
+
+
+import unach.trabajogrupal2.entidades.*;
+import java.awt.BorderLayout;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.*;
+import unach.trabajogrupal2.dao.*;
+import unach.trabajogrupal2.impl.*;
+import unach.trabajogrupal2.entidades.*;
+import java.util.List;
+
+public class frmNuevoCliente extends JInternalFrame{
+    
+    JLabel lblCodigo;
+    JLabel lblCedula;
+    JLabel lblNombres;
+    JLabel lblApellidos;
+    JLabel lblTitulo0;
+    
+    JTextField txtCodigo;
+    JTextField txtCedula;
+    JTextField txtNombres;
+    JTextField txtApellidos;
+    
+   
+    
+    JButton btnLimpiar;
+    JButton btnAceptar;
+    
+    JPanel pnlCentral;
+    JPanel pnlPie;
+    public frmNuevoCliente() {
+        this.setSize(300, 300);
+        this.setLayout(new BorderLayout());
+        this.setClosable(true);
+        pnlCentral= new JPanel();
+        pnlPie= new JPanel();
+        pnlCentral.setLayout(new GridLayout(10, 2, 5, 5));
+        pnlPie.setLayout(new GridLayout(1,2,5,5));
+        
+        lblTitulo0 = new JLabel("Datos Cliente");
+        
+        lblCodigo= new JLabel("Código:");
+        lblCedula= new JLabel("Cédula:");
+        lblNombres= new JLabel("Nombres:");
+        lblApellidos= new JLabel("Apellidos:");
+        
+        txtCodigo = new JTextField(2);
+        txtCedula= new JTextField(2);
+        txtNombres= new JTextField(2);
+        txtApellidos= new JTextField(2);
+       
+        
+        btnLimpiar= new JButton("Limpiar");
+        btnAceptar= new JButton("Aceptar");
+        
+        pnlCentral.add(lblCodigo);
+        pnlCentral.add(txtCodigo);
+        pnlCentral.add(lblCedula);
+        pnlCentral.add(txtCedula);
+        pnlCentral.add(lblNombres);
+        pnlCentral.add(txtNombres);
+        pnlCentral.add(lblApellidos);
+        pnlCentral.add(txtApellidos);
+        
+        
+                
+        btnAceptar.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                try {
+                    btnAceptarActionListener(e);
+                } catch (Exception ex) {
+                    System.out.println("Error:"+ex.getMessage());
+                }
+            }
+        });
+        
+        pnlPie.add(btnLimpiar);
+        pnlPie.add(btnAceptar);
+        
+        this.add(lblTitulo0, BorderLayout.NORTH);
+        this.add(pnlCentral, BorderLayout.CENTER);
+        this.add(pnlPie, BorderLayout.SOUTH);        
+    }
+    public static void main(String[] args) {
+        frmNuevoCliente frmMenu= new frmNuevoCliente();
+        frmMenu.setVisible(true);
+    } 
+    
+    
+    public void btnAceptarActionListener(ActionEvent e){
+        JOptionPane.showMessageDialog(this,"Proceso correcto!!",
+                "Transacción", JOptionPane.INFORMATION_MESSAGE);
+    }
+}
+ 
